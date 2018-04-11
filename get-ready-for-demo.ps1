edcb66f818eda0444542728dc89147a9a98b0827
Ashley Poole <git@ashleypoole.co.uk>
2018-04-11 20:56:42 +0100

    Update get ready script

--- get-ready-for-demo.ps1
+++ get-ready-for-demo.ps1
@@ -3,20 +3,23 @@ docker rm $(docker ps -a -q)
 docker rmi $(docker images -q)
 
 # Pull base images
-docker pull microsoft/powershell:latest
+docker pull microsoft/powershell:nanoserver
 docker pull microsoft/aspnetcore:2.0.0-nanoserver
 docker pull microsoft/aspnetcore-build:2.0.0-nanoserver
 docker pull microsoft/mssql-server-windows-developer:2017
 
 # Pull backup of demo images
+docker pull hello-world
 docker pull ashleypoole/hellodocker101
 docker pull ashleypoole/musicstoredb
 docker pull ashleypoole/musicstore
 
 # Save backup images
 New-Item ImageBackups -ItemType directory
-Set-Location -Path ImageBackups
+Push-Location ImageBackups
 
 docker save ashleypoole/hellodocker101 > hellodocker101.tar
 docker save ashleypoole/musicstoredb > musicstoredb.tar
 docker save ashleypoole/musicstore > musicstore.tar
+
+Pop-Location
